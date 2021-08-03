9e9d8b5c382c0bcef5bb1d479b8a32bf0c30c7a2
hyf-github-user <1348977728@qq.com>
2021-08-03 14:53:21 +0800

    蓝图的初始配置配置

---
+++ bluelog/blueprints/admin.py
@@ -0,0 +1,5 @@
+# 作者：我只是代码的搬运工
+# coding:utf-8
+from flask import Blueprint
+
+admin_bp = Blueprint('admin', __name__)
---
+++ bluelog/blueprints/blog.py
@@ -0,0 +1,6 @@
+# 作者：我只是代码的搬运工
+# coding:utf-8
+from flask import Blueprint
+
+
+blog_bp = Blueprint('blog', __name__)
