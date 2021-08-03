0987a8ba8cb1964d98fb5aaa143c0614aa86859f
hyf-github-user <1348977728@qq.com>
2021-08-03 14:41:26 +0800

    初始化数据库配置

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ manager = Manager(app=app)  # flask-script命令
 
 # 数据库命令的配置
 migrate = Migrate(app=app, db=db)
-# 把命令添加到flask-script命令,MigrateCommand这个只有flask_migrate版本是2.5.3才具有的
+# 把命令添加到flask-script命令,MigrateCommand这个只有flask_migrate版本是2.5.3才具有的 此时的flask的版本必须是1.1.2
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
