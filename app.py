da1e608c4c9234d80a07e2481e929696e2236d30
hyf-github-user <1348977728@qq.com>
2021-08-03 18:30:43 +0800

    加入404与500页面

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
@@ -13,6 +14,19 @@ manager = Manager(app=app)  # flask-script命令
 migrate = Migrate(app=app, db=db)
 # 把命令添加到flask-script命令,MigrateCommand这个只有flask_migrate版本是2.5.3才具有的 此时的flask的版本必须是1.1.2
 manager.add_command('db', MigrateCommand)
+
+
+# 设置404页面
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html')
+
+
+# 设置500页面
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('errors/500.html')
+
 
 if __name__ == '__main__':
     manager.run()
